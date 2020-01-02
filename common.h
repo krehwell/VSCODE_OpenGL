48145c961e9cb200925e22664bc9a0f996bfd817
kel <fahrimuham001@gmail.com>
2020-01-02 12:23:07 +0800

    init

---
+++ common.h
@@ -0,0 +1,30 @@
+#ifndef _COMMON_H
+#define _COMMON_H
+
+/*=======STANDARD TEMPLATE LIBRARY=======*/
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <memory>
+#include <ctime>
+#include <vector>
+#include <windows.h>
+
+using namespace std;
+
+#define GLUT_DISABLE_ATEXIT_HACK
+/*=======GLU/GLEW=======*/
+#include <GL/glew.h>
+#include <GL/glu.h>
+#include <GL/gl.h>
+#include <GL/glut.h>
+
+/*=======SDL2=======*/
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
+
+/*=======CLASS DEFINITIONS=======*/
+
+#endif
